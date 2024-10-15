e724f960711f4488ae29e8308755a7584081dea1
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-10-15 09:42:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Efecto de menú hamburguesa
+/ Efecto de menú hamburguesa
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('open');
+    document.querySelector('.nav-links').classList.toggle('active');  // Cambié 'open' por 'active'
 });
 
 // Funcionalidad del modal
@@ -14,17 +14,18 @@ document.querySelectorAll('.gallery-item').forEach(item => {
 
     // Añade un evento 'click' a cada elemento de la galería
     item.addEventListener('click', () => {
-         // Cambia la propiedad 'src' del iframe al valor del atributo 'data-project' del elemento clicado,
+        // Cambia la propiedad 'src' del iframe al valor del atributo 'data-project' del elemento clicado,
         // cargando así el contenido correspondiente al proyecto seleccionado
         projectFrame.src = item.getAttribute('data-project');
 
-         // Muestra el modal configurando su estilo 'display' a 'flex', lo que lo hace visible
+        // Muestra el modal configurando su estilo 'display' a 'flex', lo que lo hace visible
         modal.style.display = 'flex';
     });
 });
+
 // Añade un evento 'click' al botón de cierre del modal (representado por el elemento con la clase 'close')
 document.querySelector('.close').addEventListener('click', () => {
-
     // Oculta el modal configurando su estilo 'display' a 'none', lo que lo hace invisible
     modal.style.display = 'none';
 });
+
